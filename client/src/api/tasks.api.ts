2280b308d1994973a09b4610df5d1aa1b7bafaf2
Miguel Angel Sanchez Papalotzi <msanchezpapalot@gmail.com>
2024-07-29 23:47:04 -0600

    agregando funcionalidad de crear tarea

--- client/src/api/tasks.api.ts
+++ client/src/api/tasks.api.ts
@@ -1,5 +1,12 @@
 import axios from "axios"
+import { Task } from "../types"
 
-export const getAllTasks = () => {
-   return axios.get('http://localhost:8000/tasks/api/v1/tasks/')
-}
+const tasksApi = axios.create({
+   baseURL: 'http://localhost:8000/tasks/api/v1/tasks/'
+})
+
+export const getAllTasks = () => tasksApi.get('/')
+
+
+export const createTask = (task:Task) => tasksApi.post('/', task)
+
--- client/src/pages/TaskFormPage/TaskFormPage.tsx
+++ client/src/pages/TaskFormPage/TaskFormPage.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useForm } from "react-hook-form"
+import { createTask } from "../../api/tasks.api"
+import { useNavigate } from "react-router-dom"
 
 export default function TaskFormPage() {
+
+  const {register, handleSubmit, formState: {
+    errors
+  }} = useForm()
+  const navigate = useNavigate()
+
+  const onSubmit = handleSubmit(async data => {
+    await createTask(data)
+    navigate('/task')
+  })
+
   return (
     <div>
-      <h1>TaskForm</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="">Tittle:</label>
+        <input type="text" placeholder='title' {...register('title', {required: true})} />
+        {errors.title && <span>this field is required</span>}
+        <label htmlFor="">Description:</label>
+        <textarea rows={3} placeholder='Description' {...register('description', {required: true})}></textarea>
+        {errors.description && <span>this field is required</span>}
+        <button>Save</button>
+      </form>
     </div>
   )
 }
